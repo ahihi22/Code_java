06d329e4f852740bae62656617be1406392c8aef
ahihi22 <88736786+ahihi22@users.noreply.github.com>
2022-01-17 17:07:37 +0700

    Ngay_17/1
    Bai_thuc_hanh_17-1

---
+++ Bai_thuc_hanh_17-1/Giai_phuong_trinh_bac_2.java
@@ -0,0 +1,40 @@
+package Bai_thuc_hanh_so_2;
+
+public class Giai_phuong_trinh_bac_2 {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        System.out.prinfln("a= "); float a=s.nextFloat();
+        System.out.prinfln("b= "); float b=s.nextFloat();
+        System.out.prinfln("c= "); float c=s.nextFloat();
+        if(a==0){
+            if(b==0){
+                if(c==0)
+                    System.out.println("PT VSN");
+                    else System.out.println("PT vo nghiem");
+
+            }
+            else{
+                float x=(float)(-b)/c;
+                System.out.println("Nghiem cua phuong trinh la x=%.1f",x);
+
+            }
+        }
+        else{
+            float delta=b*b-4*a*c;
+            if(delta<0){
+                System.out.println("PT VO NGHIEM");
+
+            }
+            else if(delta==0){
+                System.out.println("PT CO NGHIEM KEP x=%.1f",-b/(2*a));
+            }
+            else{
+                float x1=(float)(((-b)+Math.sqrt(delta))/2*a);
+                float x2=(float)(((-b)-Math.sqrt(delta))/2*a);
+                System.out.println("x1=%.1f",x1);
+                System.out.println("x1=%.1f",x2);
+
+            }
+        }
+    }
+}
---
+++ Bai_thuc_hanh_17-1/Ngay_2.java
@@ -0,0 +1,16 @@
+package Bai_thuc_hanh_so_2;
+
+import java.util.Scanner;
+
+public class Ngay_2 {
+
+        public static void main(String[] args) {
+            Scanner a=new Scanner(System.in);
+            String HoTen="";
+            System.out.println("Nhap ho ten: ");
+            HoTen=a.nextLine();
+            System.out.println("Xin chao "+HoTen);
+        }
+    }
+
+}
